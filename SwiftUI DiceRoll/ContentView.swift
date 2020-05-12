c4458786df08bbbdc12637676bba1926ca8b921d
d4rkz3r0 <d4rkz3r0@users.noreply.github.com>
2020-05-12 19:33:39 -0400

    Initial Commit

---
+++ SwiftUI DiceRoll/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI DiceRoll
+//
+//  Created by Steve Kerney on 5/12/20.
+//  Copyright © 2020 Steve Kerney. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
