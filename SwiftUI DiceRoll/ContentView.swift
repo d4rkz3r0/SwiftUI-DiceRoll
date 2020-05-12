2eb10ff12b967b4e995232038a44aacd7fa468d4
d4rkz3r0 <d4rkz3r0@users.noreply.github.com>
2020-05-12 19:37:26 -0400

    Setup initial UI and add assets.

--- SwiftUI DiceRoll/ContentView.swift
+++ SwiftUI DiceRoll/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  SwiftUI DiceRoll
+//  DiceRollSwiftUI
 //
 //  Created by Steve Kerney on 5/12/20.
 //  Copyright © 2020 Steve Kerney. All rights reserved.
@@ -10,12 +10,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("logo")
+                Spacer()
+                HStack {
+                    DiceView(imageIndex: "1")
+                    DiceView(imageIndex: "2")
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+
+                }) {
+                    Text("ROLL!")
+                        .font(.system(size: 50))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }.background(Color.red)
+
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    let imageIndex: String
+
+    var body: some View {
+        Image("dice\(imageIndex)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
